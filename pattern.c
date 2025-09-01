76030bfb10b36c009d071087ef095dd47e8dc063
Penchala Vamsi Chebrolu <69138538+penchalavamsic@users.noreply.github.com>
2025-09-01 08:50:08 +0530

    patterns

---
+++ pattern.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int n, i, j;
+    printf("Enter a number\n");
+    scanf("%d", &n);
+    for(i=0;i<n;i++){
+        for(j=0;j<=i;j++){
+            printf("%d ", i);
+        }
+        printf("\n");
+    
+    }
+    return 0;
+
+}
